142f2461985058159a9962ed6f1a2726a1720e7c
McKye Gadke <mckye.gadke@gmail.com>
2019-11-29 03:10:02 +0000

    entering number of words - make it selectable to only integers #30

--- madlib.php
+++ madlib.php
@@ -1,37 +1,26 @@
 <!DOCTYPE html>
 <html>
 <style>
-    body{
+    body
+	{
         background-color:#6699ff;
-}
+	}
 </style>
 <body>
+
+<form method="post" action="createtemplate.php" >
+	<label for="howmanywords">Number of words (2-20):&nbsp;&nbsp;&nbsp;</label>
+
 <?php
-
 $boxtext = rand(3,20);
-echo "<form method=\"post\" action=\"createtemplate.php\" >";
-echo "\n";  
-echo "\n";
-echo "    How many words?";
-echo "\n";  
-echo "    <textarea name=\"howmanywords\">";
-echo "\n";
-echo $boxtext;
-echo "\n";  
-echo "    </textarea>";
-echo "<br>";
-echo "<br>";
-echo "\n";  
-echo "    <input type=\"submit\" value=\"Submit word selection.\">";
-echo "\n";      
-echo "    </form>";
-
+echo "    <input type=\"number\" id=\"howmanywords\" name=\"howmanywords\" value=\"$boxtext\" min=\"2\" max=\"20\">\n";
 ?>
-
+	<br>
+	<br>
+    <input type="submit" value="Submit word selection.">
+</form>
 <br>
 <br>
 <a href="dumpdb.php">Dump db</a>
-
 </body>
-
 </html>
